0a66fcdd7169b24aa22ffdf1da370883ae58fdd2
김나린 <knl_1219@hanmail.net>
2022-02-09 13:24:34 +0900

    Feat:Email 인증 DB 추가

--- backend/pet/src/main/java/com/sns/pet/controller/UserController.java
+++ backend/pet/src/main/java/com/sns/pet/controller/UserController.java
@@ -198,10 +198,10 @@ public class UserController {
         return new ResponseEntity<List<UserDto>>(userDtoList, HttpStatus.OK);
     }
 
-    @ApiOperation(value = "회원가입 인증메일 보내기", notes = "메일을 보내고, 인증키를 반환한다.")
-    @PostMapping("/emailConfirm/{userEmail}")
-    public ResponseEntity<String> emailConfirm(@PathVariable("userEmail") @ApiParam(value = "이메일 주소") String userEmail) throws Exception {
-        logger.info("emailConfirm 호출");
+    @ApiOperation(value = "회원가입 인증메일 보내기", notes = "메일을 보내고, DB에 인증키 정보를 저장한다. 성공시 SUCCESS, 실패시 FAIL 반환")
+    @PostMapping("/sendEmail/{userEmail}")
+    public ResponseEntity<String> sendEmail(@PathVariable("userEmail") @ApiParam(value = "이메일 주소") String userEmail) throws Exception {
+        logger.info("sendEmail 호출");
         String authKey = emailService.sendSimpleMessage(userEmail);
         System.out.println(authKey);
         if(emailService.addAuthKey(userEmail, authKey)){
@@ -211,7 +211,7 @@ public class UserController {
         }
     }
 
-    @ApiOperation(value = "이메일 인증")
+    @ApiOperation(value = "이메일 인증", notes = "입력한 인증키와 저장된 인증키가 같은지 비교 후, 같다면 SUCCESS, 다르면 FAIL 반환")
     @PostMapping("/emailConfirm")
     public ResponseEntity<String> emailConfirm(@RequestBody EmailDto emailDto) throws Exception {
         logger.info("emailConfirm 호출");
