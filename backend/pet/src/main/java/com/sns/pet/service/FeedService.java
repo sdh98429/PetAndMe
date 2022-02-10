1aed304b4c91b1d9a047240fccb76cda44f78468
이주형 <ssrcus27@naver.com>
2022-02-10 23:55:59 +0900

    Feat:뉴스피드 수정

--- backend/pet/src/main/java/com/sns/pet/service/FeedService.java
+++ backend/pet/src/main/java/com/sns/pet/service/FeedService.java
@@ -4,16 +4,17 @@ import com.sns.pet.dto.CommentDto;
 import com.sns.pet.dto.FeedDto;
 import com.sns.pet.dto.FeedPhotoDto;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface FeedService {
-    List<FeedDto> findNewsFeedList(Long userNumber, String cursor) throws Exception;
-    List<FeedDto> findFavFeedList(Long userNumber, String cursor) throws Exception;
-    List<FeedDto> findFollowFeedList(Long userNumber, String cursor) throws Exception;
-    List<FeedDto> findMyFeedList(Long userNumber) throws Exception;
-    boolean addFeed(FeedDto feedDto) throws Exception;
-    FeedDto findFeed(Long userNumber, Long feedNumber) throws Exception;
-//    boolean modifyFeed(FeedDto feedDto) throws Exception;
-    boolean removeFeed(Long feedNumber) throws Exception;
-    List<FeedPhotoDto> listImage(int[] feedNumbers) throws Exception;
+    List<FeedDto> findNewsFeedList(Long userNumber, String cursor) throws SQLException;
+    List<FeedDto> findFavFeedList(Long userNumber, String cursor) throws SQLException;
+    List<FeedDto> findFollowFeedList(Long userNumber, String cursor) throws SQLException;
+    List<FeedDto> findMyFeedList(Long userNumber) throws SQLException;
+    boolean addFeed(FeedDto feedDto) throws SQLException;
+    FeedDto findFeed(Long userNumber, Long feedNumber) throws SQLException;
+//    boolean modifyFeed(FeedDto feedDto) throws SQLException;
+    boolean removeFeed(Long feedNumber) throws SQLException;
+    List<FeedPhotoDto> listImage(int[] feedNumbers) throws SQLException;
 }
