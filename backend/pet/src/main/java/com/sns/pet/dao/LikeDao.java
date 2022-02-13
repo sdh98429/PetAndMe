ecab4be04e9e198b62a6461e71d0760a2006f2c1
이주형 <ssrcus27@naver.com>
2022-02-13 22:09:51 +0900

    Comment: 주석 및 swagger 추가

--- backend/pet/src/main/java/com/sns/pet/dao/LikeDao.java
+++ backend/pet/src/main/java/com/sns/pet/dao/LikeDao.java
@@ -8,7 +8,10 @@ import java.util.List;
 
 @Mapper
 public interface LikeDao {
+    // 좋아요 생성
     int insertLike(Long userNumber, Long feedNumber) throws SQLException;
+    // 좋아요 삭제
     int deleteLike(Long userNumber, Long feedNumber) throws SQLException;
+    // 해당 피드 좋아요 조회
     List<UserDto> selectLikeList(Long feedNumber) throws SQLException;
 }
--- backend/pet/src/main/java/com/sns/pet/dto/CommentDto.java
+++ backend/pet/src/main/java/com/sns/pet/dto/CommentDto.java
@@ -1,18 +1,29 @@
 package com.sns.pet.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
 public class CommentDto {
+    @ApiModelProperty(value = "피드 번호")
     private Long feedNumber;
+    @ApiModelProperty(value = "댓글 번호")
     private Long commentNumber;
+    @ApiModelProperty(value = "댓글 내용")
     private String commentContent;
+    @ApiModelProperty(value = "댓글 생성일")
     private String commentDate;
+    @ApiModelProperty(value = "댓글 등록자 번호(유저 번호)")
     private Long userNumber;
+    @ApiModelProperty(value = "댓글 등록자 ID(유저 ID)")
     private String userID;
+    @ApiModelProperty(value = "댓글 등록자 닉네임(유저 닉네임)")
     private String userNickName;
+    @ApiModelProperty(value = "댓글 등록자 프로필 이름(유저 프로필 이름)")
     private String userPhotoName;
+    @ApiModelProperty(value = "댓글 등록자 프로필 저장 폴더(유저 프로필 저장 폴더)")
     private String saveFolder;
+    @ApiModelProperty(value = "댓글 등록자 프로필 사진")
     private byte[] userProfilePhoto;
 }
